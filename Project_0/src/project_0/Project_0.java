ec4080369bb7fa18400236d1844ea35de0ec9af1
bennytalks <bennytoeppe@gmail.com>
2021-05-15 13:36:50 -0400

    Create Project_0.java

---
+++ Project_0/src/project_0/Project_0.java
@@ -0,0 +1,24 @@
+
+package project_0;
+
+/**
+ *
+ * @author Benny
+ */
+public class Project_0 {
+
+    public static void main(String[] args) {
+       System.out.println("Rachel Benny Toeppe \n"
+               + "Call me Benny please! \n"
+               + "(they/them) pronouns \n"
+               + "software engineering major at U of M dearborn\n"
+               + "I'm taking this course for transfer credit \n"
+               + "I'm hoping to learn some new stuff! \n \n"
+               + ""
+               + "I work full time as a Preschool teacher, So you won't be able to \n"
+               + "reach me until 5pm most days, or early in the morning.\n"
+               + "I also went to art school for computer animation before this, so I am hoping\n"
+               + " to one day become a techincal director for a feature film.");
+    }
+    
+}
